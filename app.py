71904c3a067287727710017ac1946689ff74a7d6
Tobias Karuth <tobiaskaruth@gmail.com>
2022-10-02 15:31:55 +0200

    index user name + email

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 #from werkzeug.security import check_password_hash, generate_password_hash
 
