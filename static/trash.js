19847f2b6642ad2fa710216c093648fced2bdbd7
Tobias Karuth <tobiaskaruth@gmail.com>
2022-10-05 16:17:44 +0200

    alert message

---
+++ static/trash.js
@@ -0,0 +1,36 @@
+function message(){
+    var first_name = document.getElementById("first_name");
+    var last_name = document.getElementById("last_name");
+    var email = document.getElementById("email");
+    var city = document.getElementById("city");
+    var state = document.getElementById("state");
+    var zip = document.getElementById("zip");
+    var street = document.getElementById("street");
+    var house_number = document.getElementById("house_number");
+    const success = document.getElementById("success");
+    const danger = document.getElementById("danger");
+
+    if(first_name.value === "" || last_name.value === "" || email.value === "" || city.value === "" || state.value === "" || zip.value === "" || street.value === "" || house_number.value === "") {
+        danger.style.display = "block";
+    }
+    else {
+        setTimeout(() => {
+            first_name.value = "";
+            last_name.value = "";
+            email.value = "";
+            city.value = "";
+            state.value = "";
+            zip.value = "";
+            street.value = "";
+            house_number.value = "";
+        }, 2000);
+
+        success.style.display = "block";
+    }
+    
+
+    setTimeout(() => {
+        danger.style.display = "none";
+        success.style.display = "none";
+    }, 4000);
+}
